49e259144ebae4a702b413d7141f011c08194e0a
Ulisses Parola <ulissesmt@gmail.com>
2018-01-11 09:24:24 -0200

    initial commit

---
+++ Assets/Prefabs/Ball/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public float Force;
+
+    private Rigidbody _ballRigidbody;
+    private AudioSource _audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _ballRigidbody = GetComponent<Rigidbody>();
+
+        Launch();
+    }
+
+    private void Launch()
+    {
+        Vector3 newForce = new Vector3(0f, 0f, Force);
+        _ballRigidbody.AddForce(newForce, ForceMode.Impulse);
+        _audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+}
---
+++ Assets/Prefabs/Main Camera/MainCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public Ball Ball;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = Ball.transform.position - transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Ball.transform.position.z < 16)
+        {
+            FollowBallPosition();
+        }
+        else
+        {
+            ZoomOut();
+        }
+        
+    }
+
+    void FollowBallPosition()
+    {
+        
+        transform.SetPositionAndRotation(Ball.transform.position - offset, this.transform.rotation);
+    }
+
+    void ZoomOut()
+    {
+        transform.SetPositionAndRotation(new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.01f), transform.rotation);
+    }
+}
